2cdc292b6b7371fe41a44a48a8823660f04aeee2
nikdsgn <nikdsgn@gmail.com>
2020-03-05 16:37:45 +0100

    2 binaries uploaded

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+  clickCounter: number = 0;
+  name: string = "";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  countClick() {
+    this.clickCounter += 1;
+  }
+
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 2,
+      midactive: this.clickCounter <= 4 && this.clickCounter > 2
+    };
+    return myClasses;
+  }
+}
